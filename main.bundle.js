97584d0bbbd43bbddddfd3a1e4ab215fd3517a07
Hector <hi@hectorarce.es>
2017-08-23 17:26:59 +0200

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -1423,8 +1423,12 @@ var TopbarModule = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: true
+    production: false
 };
 //# sourceMappingURL=environment.js.map
 
